c3084ce3fd104cf9357abe23b5c976c5eb853bc8
Debanjan Kauri <debanjankauri001@gmail.com>
2025-05-07 13:46:35 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBhxm9igIyBCbuuA7MzDMdgrIsEdWI-EyA",
-    authDomain: "digischolar-b7138.firebaseapp.com",
-    projectId: "digischolar-b7138",
-    storageBucket: "digischolar-b7138.appspot.com",
-    messagingSenderId: "897820565743",
-    appId: "1:897820565743:web:a74398241dbaf483f63b1a",
-    measurementId: "G-WH6ZZF3MQX"
+    apiKey: "YOUR-API-KEY",
+    authDomain: "YOUR-AUTH-DOMAIN",
+    projectId: "YOUR-PROJECT-ID",
+    storageBucket: "YOUR-STORAGE-BUCKET",
+    messagingSenderId: "YOUR-MESSAGINGSENDER-ID",
+    appId: "YOUR-APP-ID",
+    measurementId: "YOUR-MEASUREMENT-ID"
 };
 
 const app = initializeApp(firebaseConfig);
